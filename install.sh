0648fc9f30c70ae279053cbb1e811c22614a1015
Arnaud Berthomier <oz@cyprio.net>
2013-03-27 15:36:46 -0600

    Fix awk expression to get first char of a string
    I *think* you meant `substr($0, 0, 1)` to get the first character of a string.
    Also, use less-wrong-ish english. ;)

--- install.sh
+++ install.sh
@@ -3,25 +3,24 @@
 REPOSITORY_NAME='AF83/dotfiles'
 REPOSITORY_URL="https://github.com/${REPOSITORY_NAME}"
 
-# Get file listing by http and ugly grep and sed
-# Maybe if this install fail on the future you may look at this line
+# Get file list over HTTP.
 FILES=`curl -s $REPOSITORY_URL | grep js-directory-link | sed "s/.* title=\"\(.*\)\".*/\1/"`
 
 update_dotfile() {
 	FILE=$1
-	echo "Get latest version for $FILE from ${REPOSITORY_NAME}."
+	echo "Getting latest version of $FILE from ${REPOSITORY_NAME}."
 	wget -c "https://raw.github.com/${REPOSITORY_NAME}/master/$FILE" -O ~/$FILE
 }
 
 for f in $FILES; do
-	first_car=`echo $f | awk '{print substr($0,0,2)}'`
+	first_car=`echo $f | awk '{print substr($0,0,1)}'`
 	if [ "$first_car" = "." ]; then
 		if [ -f ~/$f ]; then
-			read -p "~/$f exist update it ? (y/n) : " update
+			read -p "~/$f already exists, overwrite it ? (y/n) : " update
 			if [ "$update" = "y" ]; then
 				update_dotfile $f
 			else
-				echo "Ok. Dont do nothing."
+				echo "Skipping $f."
 			fi
 		else
 			update_dotfile $f
